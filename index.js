50dc127d2ea3f18b201b86994b1468fa06652670
Srivatsan <vatsan1993@gmail.com>
2023-05-28 12:29:07 +0530

    finished the program

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-
 // Your code here
+const messages = require('./messages');
+const sayHelloTo = require('./send-messages/say-hello-to');
+const giveMessageToMrsPotato = require('./send-messages/give-message-to-mrs-potato');
 
+const msg1 = messages.message1;
+const msg2 = messages.message2;
+const msg3 = messages.message3;
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
-sayHelloTo("Mr. Potato");
+sayHelloTo('Mr. Potato');
 giveMessageToMrsPotato(msg1);
 giveMessageToMrsPotato(msg2);
 giveMessageToMrsPotato(msg3);
